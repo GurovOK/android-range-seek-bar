f6ce375609dccf54d4cee21da6162e4a507173a6
Gurov O.K <gurovok@sib-soft.ru>
2015-09-09 18:19:34 +0600

    additional customization - labels formatting

--- rangeseekbar/src/main/java/org/florescu/android/rangeseekbar/RangeSeekBar.java
+++ rangeseekbar/src/main/java/org/florescu/android/rangeseekbar/RangeSeekBar.java
@@ -668,8 +668,8 @@ public class RangeSeekBar<T extends Number> extends ImageView {
             // give text a bit more space here so it doesn't get cut off
             int offset = PixelUtil.dpToPx(getContext(), TEXT_LATERAL_PADDING_IN_DP);
 
-            String minText = String.valueOf(getSelectedMinValue());
-            String maxText = String.valueOf(getSelectedMaxValue());
+            String minText = String.format("%,d", getSelectedMinValue()).replace(",", "");
+            String maxText = String.format("%,d", getSelectedMaxValue()).replace(",", "");
             float minTextWidth = paint.measureText(minText) + offset;
             float maxTextWidth = paint.measureText(maxText) + offset;
 
